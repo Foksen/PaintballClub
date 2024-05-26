582659d777acc1ca8d83f648a5a317eb2b8d91d4
Foksen <izholobov2004@gmail.com>
2024-05-26 18:36:38 +0300

    Deploy upd

--- html/scripts/webconfig.js
+++ html/scripts/webconfig.js
@@ -27,3 +27,10 @@ async function requestToApi(route, details) {
         }));
     }
 }
+
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
